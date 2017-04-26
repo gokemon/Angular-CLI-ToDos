ce477fbbe7af910c5932ce86b358360a202a0bc8
gokemon <michael.of.gokey@gmail.com>
2017-04-25 23:41:33 -0400

    created app, added service, and lots of tests

---
+++ todo-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'My ToDo App';
+  myStatement1 = 'Things to do';
+  myStatement2 = 'English bundle';
+}
